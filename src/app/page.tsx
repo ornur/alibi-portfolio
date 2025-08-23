e99e2d6755e6b1d7806048e985f4db0af4641a44
ornur <nurdauletaidaruli@gmail.com>
2025-08-24 01:58:53 +0500

    Add Main component with introductory content in Russian and integrate it into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { About } from "@/components/about";
 import { NavBar } from "@/components/navbar";
+import { Main } from "@/components/main";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-5xl">
+    <div className="mx-auto max-w-5xl h-[1500px]">
       <NavBar />
+      <Main />
       <About />
     </div>
   );
