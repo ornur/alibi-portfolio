f632ee3dd5fa48eb0507943981be7be28db29fdd
ornur <nurdauletaidaruli@gmail.com>
2025-09-24 19:18:11 +0500

    Refactor imports in page.tsx to use section-based paths; add AboutMe, Projects, and Achievements components to Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import { HeroHeader } from "@/components/header";
-import ContactSection from "@/components/contact-form";
-import FooterSection from "@/components/footer";
-import HeroSection from "@/components/hero-section";
+import { HeroHeader } from "@/components/sections/header";
+import ContactSection from "@/components/sections/contact-form";
+import FooterSection from "@/components/sections/footer";
+import HeroSection from "@/components/sections/hero-section";
+import { AboutMe } from "@/components/sections/about-me";
+import { Projects } from "@/components/sections/projects";
+import { Achievements } from "@/components/sections/achievements";
 
 export default function Home() {
   return (
     <div className="mx-auto">
       <HeroHeader />
       <HeroSection />
+      <AboutMe />
+      <Projects />
+      <Achievements />
       <ContactSection />
       <FooterSection />
     </div>
