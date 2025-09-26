4ffca1e78d2238ebdc69f668d5c65b4015c415a1
ornur <nurdauletaidaruli@gmail.com>
2025-09-26 12:49:39 +0500

    feat: add Yurt logo to LogosBanner component with external link

--- src/components/sections/logos-banner.tsx
+++ src/components/sections/logos-banner.tsx
@@ -2,71 +2,93 @@ import { InfiniteSlider } from "@/components/motion-primitives/infinite-slider";
 import { ProgressiveBlur } from "@/components/motion-primitives/progressive-blur";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import Link from "next/link";
 
 const logos = [
   {
     src: "/logos/125_high_school.png",
     alt: "125 High School Logo",
+    link: "https://www.instagram.com/125highschool.shymkent/",
   },
   {
     src: "/logos/alash_pride.webp",
     alt: "Alash Pride Logo",
+    link: "https://www.instagram.com/alash_pridefc/",
   },
   {
     src: "/logos/capable_group.webp",
     alt: "Capable Group Logo",
+    link: "https://www.instagram.com/capable_group/",
   },
   {
     src: "/logos/fun_sun.png",
     alt: "Fun Sun Logo",
+    link: "https://www.instagram.com/fstravel_kazakhstan",
   },
   {
     src: "/logos/geely.webp",
     alt: "Geely Logo",
+    link: "https://www.instagram.com/geely_astana/",
   },
   {
     src: "/logos/hikmet.webp",
     alt: "Hikmet Logo",
+    link: "https://www.instagram.com/hickmet_astana",
   },
   {
     src: "/logos/holyland.webp",
     alt: "Holyland Logo",
+    link: "https://www.instagram.com/holyland_kz",
   },
   {
     src: "/logos/huawei.webp",
     alt: "Huawei Logo",
+    link: "https://www.instagram.com/huaweikazakhstan/",
   },
   {
     src: "/logos/jas_korermen.webp",
     alt: "Jas Korermen Logo",
+    link: "https://www.instagram.com/muztheatre.kz/",
   },
   {
     src: "/logos/myrza.png",
     alt: "Myrza Logo",
+    link: "https://www.instagram.com/myrza.brands.astana/",
   },
   {
     src: "/logos/naiza.webp",
     alt: "Naiza Logo",
+    link: "https://www.instagram.com/naiza.mma/",
   },
   {
     src: "/logos/otau.webp",
     alt: "Otau Logo",
+    link: "https://www.instagram.com/otaugroup/",
   },
   {
     src: "/logos/tari_new.webp",
     alt: "Tari Logo",
+    link: "https://www.instagram.com/tary.coffee/",
   },
   {
     src: "/logos/tumi.webp",
     alt: "Tumi Logo",
+    link: "https://www.instagram.com/tumi_kazakhstan/",
   },
   {
     src: "/logos/visit_astana.webp",
     alt: "Visit Astana Logo",
+    link: "https://www.instagram.com/visitastana.kz/",
   },
   {
     src: "/logos/volt.png",
     alt: "Volt Logo",
+    link: "https://www.instagram.com/",
+  },
+  {
+    src: "/logos/yurt.png",
+    alt: "Yurt Logo",
+    link: "https://www.instagram.com/",
   },
 ];
 
@@ -84,14 +106,15 @@ export function LogosBanner() {
           <div className="relative py-6 md:w-[calc(100%-11rem)]">
             <InfiniteSlider speedOnHover={20} speed={40} gap={100}>
               {logos.map((logo) => (
-                <Image
-                  key={logo.src}
-                  src={logo.src}
-                  alt={logo.alt}
-                  className="mx-auto flex h-12 w-26 object-contain"
-                  width={100}
-                  height={48}
-                />
+                <Link href={logo.link} className="p-0 m-0" target="_blank" key={logo.src}>
+                  <Image
+                    src={logo.src}
+                    alt={logo.alt}
+                    className="mx-auto flex h-12 w-26 object-contain"
+                    width={100}
+                    height={48}
+                  />
+                </Link>
               ))}
             </InfiniteSlider>
 
