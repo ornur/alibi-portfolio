c1609e6f3cbf4012637d04f63fbcfbc73f550819
ornur <nurdauletaidaruli@gmail.com>
2025-10-11 03:52:46 +0500

    feat: refactor LogosBanner component to improve layout and enhance logo display with multiple sliders

--- src/components/sections/logos-banner.tsx
+++ src/components/sections/logos-banner.tsx
@@ -92,25 +92,68 @@ const logos = [
   },
 ];
 
+const logos1 = logos.slice(0, Math.ceil(logos.length / 3));
+const logos2 = logos.slice(Math.ceil(logos.length / 3), Math.ceil(logos.length / 3) * 2);
+const logos3 = logos.slice(Math.ceil(logos.length / 3) * 2, logos.length);
+
 export function LogosBanner() {
   const t = useTranslations("hero");
   return (
-    <section className="mx-auto max-w-6xl overflow-x-hidden bg-background pb-16 md:pb-32">
+    <section id="companies" className="mx-auto max-w-6xl overflow-x-hidden bg-background pb-16 md:py-32">
       <div className="group relative m-auto max-w-6xl px-6">
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="md:border-r md:pr-6">
-            <p className="w-full text-end font-cyrillic text-sm text-nowrap">
-              {t("clients")}
-            </p>
-          </div>
-          <div className="relative py-6 md:w-[calc(100%-11rem)]">
-            <InfiniteSlider speedOnHover={20} speed={40} gap={100}>
-              {logos.map((logo) => (
-                <Link href={logo.link} className="p-0 m-0" target="_blank" key={logo.src}>
+        <div className="flex flex-col items-center">
+          <p className="w-full text-center font-cyrillic lg:text-5xl text-sm font-bold text-nowrap">
+            {t("clients")}
+          </p>
+          <div className="relative space-y-38 py-16 md:w-full">
+            <InfiniteSlider key={"logos1"} speedOnHover={40} speed={250} gap={200}>
+              {logos1.map((logo) => (
+                <Link
+                  href={logo.link}
+                  className="m-0 p-0"
+                  target="_blank"
+                  key={logo.src}
+                >
                   <Image
                     src={logo.src}
                     alt={logo.alt}
-                    className="mx-auto flex h-12 w-26 object-contain grayscale brightness-200"
+                    className="mx-auto flex h-24 w-32 object-contain brightness-200 grayscale"
+                    width={100}
+                    height={48}
+                  />
+                </Link>
+              ))}
+            </InfiniteSlider>
+            <InfiniteSlider key={"logos2"} speedOnHover={40} reverse speed={160} gap={200}>
+              {logos2.map((logo) => (
+                <Link
+                  href={logo.link}
+                  className="m-0 p-0"
+                  target="_blank"
+                  key={logo.src}
+                >
+                  <Image
+                    src={logo.src}
+                    alt={logo.alt}
+                    className="mx-auto flex h-24 w-32 object-contain brightness-200 grayscale"
+                    width={100}
+                    height={48}
+                  />
+                </Link>
+              ))}
+            </InfiniteSlider>
+            <InfiniteSlider key={"logos3"} speedOnHover={40} speed={160} gap={200}>
+              {logos3.map((logo) => (
+                <Link
+                  href={logo.link}
+                  className="m-0 p-0"
+                  target="_blank"
+                  key={logo.src}  
+                >
+                  <Image
+                    src={logo.src}
+                    alt={logo.alt}
+                    className="mx-auto flex h-24 w-32 object-contain brightness-200 grayscale"
                     width={100}
                     height={48}
                   />
