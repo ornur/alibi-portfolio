bb52e183c8da84c6539ba99205ac6b9499ce4381
ornur <nurdauletaidaruli@gmail.com>
2025-10-16 18:05:47 +0500

    refactor: improve layout and styling of LogosBanner component

--- src/components/sections/logos-banner.tsx
+++ src/components/sections/logos-banner.tsx
@@ -93,88 +93,97 @@ const logos = [
 ];
 
 const logos1 = logos.slice(0, Math.ceil(logos.length / 3));
-const logos2 = logos.slice(Math.ceil(logos.length / 3), Math.ceil(logos.length / 3) * 2);
+const logos2 = logos.slice(
+  Math.ceil(logos.length / 3),
+  Math.ceil(logos.length / 3) * 2
+);
 const logos3 = logos.slice(Math.ceil(logos.length / 3) * 2, logos.length);
 
 export function LogosBanner() {
   const t = useTranslations("hero");
   return (
-    <section id="companies" className="mx-auto max-w-6xl overflow-x-hidden bg-background pb-16 md:py-32">
-      <div className="group relative m-auto max-w-6xl px-6">
-        <div className="flex flex-col items-center">
-          <p className="w-full text-center font-cyrillic lg:text-5xl text-sm font-bold text-nowrap">
-            {t("clients")}
-          </p>
-          <div className="relative space-y-38 py-16 md:w-full">
-            <InfiniteSlider key={"logos1"} speedOnHover={40} speed={250} gap={200}>
-              {logos1.map((logo) => (
-                <Link
-                  href={logo.link}
-                  className="m-0 p-0"
-                  target="_blank"
-                  key={logo.src}
-                >
-                  <Image
-                    src={logo.src}
-                    alt={logo.alt}
-                    className="mx-auto flex h-24 w-32 object-contain"
-                    width={100}
-                    height={48}
-                  />
-                </Link>
-              ))}
-            </InfiniteSlider>
-            <InfiniteSlider key={"logos2"} speedOnHover={40} reverse speed={160} gap={200}>
-              {logos2.map((logo) => (
-                <Link
-                  href={logo.link}
-                  className="m-0 p-0"
-                  target="_blank"
-                  key={logo.src}
-                >
-                  <Image
-                    src={logo.src}
-                    alt={logo.alt}
-                    className="mx-auto flex h-24 w-32 object-contain"
-                    width={100}
-                    height={48}
-                  />
-                </Link>
-              ))}
-            </InfiniteSlider>
-            <InfiniteSlider key={"logos3"} speedOnHover={40} speed={160} gap={200}>
-              {logos3.map((logo) => (
-                <Link
-                  href={logo.link}
-                  className="m-0 p-0"
-                  target="_blank"
-                  key={logo.src}  
-                >
-                  <Image
-                    src={logo.src}
-                    alt={logo.alt}
-                    className="mx-auto flex h-24 w-32 object-contain"
-                    width={100}
-                    height={48}
-                  />
-                </Link>
-              ))}
-            </InfiniteSlider>
+    <section
+      id="companies"
+      className="group relative m-auto mx-auto flex flex-col items-center overflow-x-hidden bg-background px-6 pb-16 md:py-32 lg:px-0"
+      style={{ scrollbarGutter: "auto" }}
+    >
+      <p className="w-full text-center font-cyrillic text-3xl font-bold text-nowrap lg:text-6xl">
+        {t("clients")}
+      </p>
+      <div className="relative space-y-24 lg:space-y-38 pt-16 md:w-full">
+        <InfiniteSlider key={"logos1"} speedOnHover={40} speed={250} gap={200}>
+          {logos1.map((logo) => (
+            <Link
+              href={logo.link}
+              className="m-0 p-0"
+              target="_blank"
+              key={logo.src}
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                className="mx-auto flex h-24 w-32 object-contain"
+                width={100}
+                height={48}
+              />
+            </Link>
+          ))}
+        </InfiniteSlider>
+        <InfiniteSlider
+          key={"logos2"}
+          speedOnHover={40}
+          reverse
+          speed={160}
+          gap={200}
+        >
+          {logos2.map((logo) => (
+            <Link
+              href={logo.link}
+              className="m-0 p-0"
+              target="_blank"
+              key={logo.src}
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                className="mx-auto flex h-24 w-32 object-contain"
+                width={100}
+                height={48}
+              />
+            </Link>
+          ))}
+        </InfiniteSlider>
+        <InfiniteSlider key={"logos3"} speedOnHover={40} speed={160} gap={200}>
+          {logos3.map((logo) => (
+            <Link
+              href={logo.link}
+              className="m-0 p-0"
+              target="_blank"
+              key={logo.src}
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                className="mx-auto flex h-24 w-32 object-contain"
+                width={100}
+                height={48}
+              />
+            </Link>
+          ))}
+        </InfiniteSlider>
 
-            <div className="absolute inset-y-0 left-0 w-20 bg-linear-to-r from-background"></div>
-            <div className="absolute inset-y-0 right-0 w-20 bg-linear-to-l from-background"></div>
-            <ProgressiveBlur
-              className="pointer-events-none absolute top-0 left-0 h-full w-20"
-              direction="left"
-              blurIntensity={1}
-            />
-            <ProgressiveBlur
-              className="pointer-events-none absolute top-0 right-0 h-full w-20"
-              direction="right"
-              blurIntensity={1}
-            />
-          </div>
-        </div>
+        <div className="absolute inset-y-0 left-0 w-20 bg-linear-to-r from-background"></div>
+        <div className="absolute inset-y-0 right-0 w-20 bg-linear-to-l from-background"></div>
+        <ProgressiveBlur
+          className="pointer-events-none absolute top-0 left-0 h-full w-20"
+          direction="left"
+          blurIntensity={1}
+        />
+        <ProgressiveBlur
+          className="pointer-events-none absolute top-0 right-0 h-full w-20"
+          direction="right"
+          blurIntensity={1}
+        />
       </div>
     </section>
   );
